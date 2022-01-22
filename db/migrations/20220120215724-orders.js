da50370eabfbd8c8229ddd2ea3e7b7cb0452bf2e
onedrako <onedrako@gmail.com>
2022-01-22 13:45:03 -0600

    fixing migration orders

--- db/migrations/20220120215724-orders.js
+++ db/migrations/20220120215724-orders.js
@@ -1,11 +1,45 @@
 'use strict';
 
-const { OrderSchema, ORDER_TABLE } = require("./../models/order.model");
-
+const { ORDER_TABLE } = require("./../models/order.model");
+const { DataTypes, Sequelize } = require('sequelize');
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      total:{
+        type: DataTypes.VIRTUAL,
+        get(){
+          if(this.items.length> 0){
+            return this.items.reduce((total, item) => {
+              return total + (item.price * item.OrderProduct.amount)
+            },0)
+          }
+        }
+      }
+    });
   },
 
   async down (queryInterface) {
